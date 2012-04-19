664add00d8024090ca77e4be79c9a58f3dfef916
Danielle Collins <dmcollin@unca.edu>
2012-04-19 18:35:25 -0400

    added partial post code from tutorial

--- app/app.js
+++ app/app.js
@@ -16,6 +16,8 @@ var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+
 // Configuration
 
 app.configure(function(){
@@ -46,3 +48,40 @@ app.get('/signup', function(req, res){
 
 app.listen(cf.port || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.ejs', { locals: {
+        title: 'New Post'
+    }
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.ejs',
+        { locals: {
+            title: article.title,
+            article:article
+        }
+        });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
