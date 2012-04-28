47522d18b1711412a8ae1324f8de1b04db3b8396
Danielle Collins <dmcollin@unca.edu>
2012-04-28 09:37:06 -0400

    added submit.js

---
+++ app/models/submit.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; 
+
+function validatePresenceOf(value) {
+  return value && value.length;
+}
+
+
+var BlogPost = new Schema({
+ post     : String
+});
+
+var Submit = mongoose.model('Submit', BlogPost);
+
+module.exports = Submit; //then can require this in program and will have access to the submit model
